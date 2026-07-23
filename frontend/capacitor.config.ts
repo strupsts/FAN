3bbb27c573a345834532a0ece9d4b6561545d33e
strupsts <strupsts@mail.ru>
2026-07-22 19:43:55 -0600

    feat: initialize Ionic Angular frontend

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.strupsts.fan',
+  appName: 'F.A.N.',
+  webDir: 'www',
+};
+
+export default config;
